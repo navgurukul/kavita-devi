fcf6b5e628cbcf0d4bcd87454c6b814e47c5b91a
Abhishek Gupta <a@navgurukul.org>
2020-07-23 01:36:17 +0530

    ok

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const sdk = require("matrix-bot-sdk");
-const config = require('dotenv').config()
+const config = require('dotenv').config().parsed;
 
 const MatrixClient = sdk.MatrixClient;
 const SimpleFsStorageProvider = sdk.SimpleFsStorageProvider;
 const AutojoinRoomsMixin = sdk.AutojoinRoomsMixin;
+const RichReply = sdk.RichReply;
 
 // where you would point a client to talk to a homeserver
 const homeserverUrl = "https://m.navgurukul.org"; // make sure to update this with your url
@@ -26,3 +27,27 @@ client.on("room.message", handleCommand);
 // Now that the client is all set up and the event handler is registered, start the
 // client up. This will start it syncing.
 client.start().then(() => console.log("Client started!"));
+
+// This is our event handler for dealing with the `!hello` command.
+async function handleCommand(roomId, event) {
+  // Don't handle events that don't have contents (they were probably redacted)
+  if (!event["content"]) return;
+
+  // Don't handle non-text events
+  if (event["content"]["msgtype"] !== "m.text") return;
+
+  // We never send `m.text` messages so this isn't required, however this is
+  // how you would filter out events sent by the bot itself.
+  if (event["sender"] === await client.getUserId()) return;
+
+  // Make sure that the event looks like a command we're expecting
+  const body = event["content"]["body"];
+
+  // If we've reached this point, we can safely execute the command. We'll
+  // send a reply to the user's command saying "Hello World!".
+  const replyBody = "Thank you for sharing your message"; // we don't have any special styling to do.
+  const reply = RichReply.createFor(roomId, event, replyBody, replyBody);
+  reply["msgtype"] = "m.notice";
+  client.sendMessage(roomId, reply);
+}
+
